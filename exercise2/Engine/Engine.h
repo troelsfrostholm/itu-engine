8108a5cd1e2bced1d3c69ef6bd18d0e4e9961265
Frederik Williams <fwil@itu.dk>
2010-09-09 16:55:53 +0200

    OpenGL window from DLL with triagle and cube. Rotation can be controlled with keyboard.

---
+++ exercise2/Engine/Engine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace nms
+{
+	class Engine
+	{
+		public:
+			static __declspec(dllexport) void Start();
+	};
+
+}
---
+++ exercise2/Main/Main.cpp
@@ -0,0 +1,29 @@
+// Main.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+#include "stdafx.h"
+#include "Engine.h"
+
+using namespace nms;
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//Engine started. Will terminate on its own.
+	Engine::Start();
+
+	//cout << "type something and presse enter to end" << endl;
+	
+	//char c;
+	// Wait for keyboard press
+	//cin >> c;
+        
+
+	
+	return 0;
+}
+
