d6d53c636a4af5a7998cb75a99aa67c634575f80
Troels <troelsfrostholm@gmail.com>
2010-09-15 21:13:40 +0200

    Created test project, which includes boost test framework and nms_math.Trig

---
+++ nms/nms_test/main.cpp
@@ -0,0 +1,12 @@
+#include "Trig.h"
+#define BOOST_TEST_MODULE TrigTest
+
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+   //Matrix m = Matrix();
+}
