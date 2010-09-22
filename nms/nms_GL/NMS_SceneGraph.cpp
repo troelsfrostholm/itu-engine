270f974864ba586be6cc146eb64ab365e1d69129
Troels <troelsfrostholm@gmail.com>
2010-09-22 13:07:14 +0200

    Added SceneGraph class and test file

---
+++ nms/nms_GL/NMS_SceneGraph.cpp
@@ -0,0 +1,1 @@
+#include "NMS_SceneGraph.h"
---
+++ nms/nms_test/scenegraph.cpp
@@ -0,0 +1,17 @@
+#include "NMS_SceneGraph.h"
+
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace nms;
+
+BOOST_AUTO_TEST_SUITE( scenegraph );
+
+BOOST_AUTO_TEST_CASE( scenegraph_node )
+{
+	//BOOST_CHECK( approximates(1.01f, 1.00f, 0.02f) );
+	BOOST_CHECK( false );
+}
+
+BOOST_AUTO_TEST_SUITE_END();
