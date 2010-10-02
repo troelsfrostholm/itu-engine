a8f830634cc5cea1a1479a8064085d324601ad5a
Ringbo <jacob@ringbo.dk>
2010-10-02 21:09:59 +0200

    Made a hack to make sure demo1 can still run while we work on the scenegraph.

--- nms/demo/main.cpp
+++ nms/demo/main.cpp
@@ -29,6 +29,63 @@ void DrawScene();
 int main(int argc, char* argv[])
 { //Start SDL 
 	engine.NMSInit(WIDTH,HEIGHT,16,"Nemesis Engine",false);
+
+
+
+	//  HACK TO MAKE IT RUN WITHOUT SCENEGRAPH!
+	bool fullscreen = false;
+	int width = 600;
+	int height = 400;
+	int flags;
+	char* windowTitle = "Demo 2";
+	bool bpp = false;
+
+	//try to initialize SDL
+	if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 ) 
+		{ return 0; } 
+	
+	if (fullscreen) {
+		flags = SDL_OPENGL | SDL_FULLSCREEN;
+		SDL_ShowCursor(SDL_DISABLE);
+		SDL_WM_GrabInput(SDL_GRAB_ON);
+	}
+	else
+	{
+		flags = SDL_OPENGL;
+		SDL_ShowCursor(SDL_DISABLE);
+		SDL_WM_GrabInput(SDL_GRAB_ON);
+	}
+
+	SDL_WM_SetCaption(windowTitle,NULL);  //Set the name of the window
+	SDL_SetVideoMode(width, height, bpp, flags); //Set the window mode
+
+	
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glViewport(0, 0, width, height); // Set the dimensions of the viewport
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glEnable(GL_TEXTURE_2D); //Initialize OpenGl and texture mapping
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	glEnable(GL_COLOR_MATERIAL);							// Enable Smooth Shading
+	glClearDepth(1.0f);									// Depth Buffer Setup
+	glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
+	glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
+	/*glFrustum(    -0.5f,
+                0.5f,
+                -0.5f*(float)(height/width),
+                0.5f*(float)(height/width),
+                1.0f,
+                500.0f);*/
+		
+	gluPerspective(60.0, (float)width/(float)height, 1.0, width);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	//END OF HACK
+
+
+
 	NMS_Audio audioEngine;
 	ALfloat sourcePos[] = {0.0f,0.0f,0.0f};
 	ALfloat sourceVel[] = {0.0f,0.0f,0.0f};
