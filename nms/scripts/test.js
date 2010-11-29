6c95819f69ec5f06aea6ac3e82c528b55df34dad
Ringbo <jacob@ringbo.dk>
2010-11-29 04:41:41 +0100

    changed js file

--- nms/scripts/test.js
+++ nms/scripts/test.js
@@ -1,18 +1,19 @@
 Log("this is the initial log file..");
-
+/*
 setGravity(10);
-
+*/
 var md2 = new MD2();
 
 md2.LoadModel("models/drfreak/drfreak.md2","models/drfreak/drfreak.tga");
-/*
+
 var root = getRoot();
 
 var node = new GeometryNode(md2);
 
 root.addChild(node);
-*/
+/*
 function Update()
 {
     Log("This is a log file");
 }
+*/
